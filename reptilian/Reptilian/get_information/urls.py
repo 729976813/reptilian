4703f51ede321369dc56cc6cdcaa55a1ff1a8296
zhangguangyu <zhanggy@knownsec.com>
2018-12-04 04:43:20 -0500

    gggg

--- reptilian/Reptilian/get_information/urls.py
+++ reptilian/Reptilian/get_information/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+
 from django.conf.urls import include, url
 from django.views.generic import TemplateView
 from reptilian.Reptilian.get_information import views
